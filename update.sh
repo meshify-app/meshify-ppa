dfe45fccfee567f9749d65257bd89cf388630906
Alan Graham <alan@meshify.app>
2022-05-25 23:31:20 +0000

    favicon

---
+++ update.sh
@@ -0,0 +1,8 @@
+export EMAIL=info@meshify.app
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+# Release, Release.gpg & InRelease
+apt-ftparchive release . > Release
+gpg --default-key "${EMAIL}" -abs -o - Release > Release.gpg
+gpg --default-key "${EMAIL}" --clearsign -o - Release > InRelease
+
